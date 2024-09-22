5dd4ef701a85207aa3a953af3d02513aa4c4ada4
Jonathan A Hand <jhand1@my.westga.edu>
2024-09-21 21:33:45 -0400

    Added business logic for storing food.
    Originally intended to add logic for storing
    food. Food class temporarily suspended intended
    functionality to troubleshoot logic located
    elsewhere in the system. Will restore on next
    file add/commit.

--- code/PantryTracker/src/main/Main.java
+++ code/PantryTracker/src/main/Main.java
@@ -8,17 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindow.fxml"));
-        AnchorPane root = loader.load();
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Pantry Tracker");
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindow.fxml"));
+		AnchorPane root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Pantry Tracker");
+		primaryStage.show();
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- code/PantryTracker/src/view/MainWindow.java
+++ code/PantryTracker/src/view/MainWindow.java
@@ -1,10 +1,18 @@
 package view;
 
+/**
+ * Code behind for the MainWindow of the application.
+ * 
+ * @author jhand1
+ * @version 1.0
+ */
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+//import model.Food;
 
 public class MainWindow {
 
@@ -17,14 +25,56 @@ public class MainWindow {
 	@FXML
 	private ListView<String> foodListView;
 
+	/**
+	 * Adds a single food item to the pantry list. Retrieves the food name and food
+	 * type, checking if the item already exists in the list. Increases the quantity
+	 * or adds it as new if not already found. Throws exception if name or type
+	 * input fields are invalid due to being empty.
+	 */
 	@FXML
 	public void handleAddFood() {
-		String foodName = foodNameTextField.getText();
-		String foodType = foodTypeComboBox.getValue();
-		if (foodName != null && !foodName.isEmpty() && foodType != null) {
-			String foodItem = foodName + " (" + foodType + ")";
-			foodListView.getItems().add(foodItem);
-			foodNameTextField.clear();
+		try {
+			String foodName = foodNameTextField.getText();
+			String foodType = foodTypeComboBox.getValue();
+			if (foodName != null && !foodName.isEmpty() && foodType != null) {
+//				Food newFood = new Food(foodName, foodType);
+//
+//				boolean wasFound = false;
+//				for (Food food : foodListView.getItems()) {
+//					if (food.getName().equals(newFood.getName()) && food.getType().equals(newFood.getType())) {
+//						food.incrementQuantity();
+//						wasFound = true;
+//						break;
+//					}
+//				}
+//
+//				if (!wasFound) {
+//					foodListView.getItems().add(newFood);
+//				}
+
+				foodListView.refresh();
+				foodNameTextField.clear();
+			} else {
+				throw new IllegalArgumentException("Food name and type must not be empty.");
+			}
+		} catch (IllegalArgumentException errorObject) {
+			showErrorAlert("Invalid Input", errorObject.getMessage());
+		} catch (Exception errorGeneral) {
+			showErrorAlert("Unexpected Error", "An unexpected error has occurred. Please check inputs.");
 		}
+	}
+
+	/**
+	 * Displays an error alert message to the user with given title and message.
+	 * 
+	 * @param title   the alert title displayed.
+	 * @param message the alert message displayed.
+	 */
+	private void showErrorAlert(String title, String message) {
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.showAndWait();
 	}
 }
