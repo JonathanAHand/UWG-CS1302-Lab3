cacd57a778472632928d1e4d2edd59c629d5227c
Jonathan A Hand <jhand1@my.westga.edu>
2024-11-20 13:57:36 -0500

    Added MenuBar to GUI with File menu/functionality
    Created new MenuBar named File to MainWindow with
    MenuItem About and corresponding handling method
    to display a popup that describes the project and lists
    the author.
    Implemented new additional MenuItem Close and method
    to handle exiting the application.
    which exits the application and closes it.

--- code/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -2,45 +2,71 @@ package edu.westga.cs1302.password_generator.view;
 
 import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-/** Codebehind for the MainWindow of the Application.
+/**
+ * Codebehind for the MainWindow of the Application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class MainWindow {
 
-    @FXML private CheckBox mustIncludeDigits;
-    @FXML private CheckBox mustIncludeLowerCaseLetters;
-    @FXML private CheckBox mustIncludeUpperCaseLetters;
-    @FXML private TextField minimumLength;
-    @FXML private TextArea output;
-    @FXML private Label errorTextLabel;
-    @FXML private Button generatePasswordButton;
-    
-    private ViewModel vm;
-    
-    @FXML
-    void initialize() {
-    	this.vm = new ViewModel();
-    	this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
-    	this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
-    	this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
-    	this.minimumLength.setText(this.vm.getMinimumLength().getValue());
-    	this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
-    	
-    	this.output.textProperty().bind(this.vm.getPassword());
-    	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
-    	
-    	this.generatePasswordButton.setOnAction(
-    			(event) -> { 
-    				this.vm.generatePassword();
-    			} 
-    	);
-    }
+	@FXML
+	private CheckBox mustIncludeDigits;
+	@FXML
+	private CheckBox mustIncludeLowerCaseLetters;
+	@FXML
+	private CheckBox mustIncludeUpperCaseLetters;
+	@FXML
+	private TextField minimumLength;
+	@FXML
+	private TextArea output;
+	@FXML
+	private Label errorTextLabel;
+	@FXML
+	private Button generatePasswordButton;
+	@FXML
+	private MenuItem fileAbout;
+	@FXML
+	private MenuItem fileClose;
+
+	private ViewModel vm;
+
+	@FXML
+	void initialize() {
+		this.vm = new ViewModel();
+		this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
+		this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
+		this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
+		this.minimumLength.setText(this.vm.getMinimumLength().getValue());
+		this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
+
+		this.output.textProperty().bind(this.vm.getPassword());
+		this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
+
+		this.generatePasswordButton.setOnAction((event) -> {
+			this.vm.generatePassword();
+		});
+	}
+
+	@FXML
+	private void handleFileAbout() {
+		Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION);
+		aboutAlert.setTitle("About");
+		aboutAlert.setHeaderText(null);
+		aboutAlert.setContentText("This program usually generates random passwords based on specified criteria.\nAuthor: Jonathan Hand");
+		aboutAlert.showAndWait();
+	}
+
+	@FXML
+	private void handleFileClose() {
+		System.exit(0);
+	}
 }
